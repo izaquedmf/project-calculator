dbb4478322412884e77d8b10dad2dc1ba9f4f2f1
izaquedmf <izaquedmf@gmail.com>
2025-03-24 19:53:06 -0300

    create operation function

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function multiply(num1, num2){
 function divide(num1,num2){
     return num1/num2;    
 }
+
+function operation(operator, num1, num2){
+    switch (operator){
+        case '+':
+            return add(num1,num2);            
+        case '-':
+            return subtract(num1,num2);         
+        case 'x':
+            return multiply(num1,num2);
+        case '/':
+            return divide(num1,num2);
+    }
+}
