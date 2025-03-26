0b4a839ee9ab700e1417ef4d88356123400d3dd4
izaquedmf <izaquedmf@gmail.com>
2025-03-26 07:10:31 -0300

    start css to organize buttons and add = button

--- script.js
+++ script.js
@@ -31,12 +31,51 @@ function operation(operator, num1, num2){
     }
 }
 
+
 function startCalculator(){
     const buttons = document.querySelector("#keyboard");
+    const numbers = document.createElement("div");
+    numbers.id = "numbers";
+
     for (let i=0; i<=9;i++){
-        let btn = document.createElement("button");
-        btn.textContent = i;
-        buttons.appendChild(btn);
+        if(i===0){
+            let btn = document.createElement("button");
+            btn.textContent = 'AC';                               
+            numbers.appendChild(btn);
+
+            btn = document.createElement("button");
+            btn.classList.add("number")
+            btn.textContent = i;
+            numbers.appendChild(btn);
+
+            btn = document.createElement("button");
+            btn.textContent = '.';        
+            numbers.appendChild(btn);
+
+        }else{
+            let btn = document.createElement("button");
+            btn.textContent = i;
+            numbers.appendChild(btn);
+        }     
     }
+
+    buttons.appendChild(numbers);
+
+    //create div operatores
+    const op = ["/", "*", "-", "+","="];
+    const operators = document.createElement("div");
+    operators.id = "allOperators";
+
+    op.forEach(item =>{
+        const btn = document.createElement("button");
+        btn.textContent = item;
+        btn.classList.add("operator");
+        operators.appendChild(btn);
+
+    })
+
+    buttons.appendChild(operators);
+
+
 }
 
